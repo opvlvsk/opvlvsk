3e5993e86fac114583e893e837760c233e685efb
olya <olapavlovska@live.ru>
2018-12-05 21:24:29 +0200

    like button

--- slider.js
+++ slider.js
@@ -4,5 +4,35 @@
       		autoplaySpeed: 3000,
       		dots:true
       	});
-      
-    });
+     });
+
+
+// function changeLikeOnClick(elmnt, backgroundImage, backgroundColor) {
+// 	if (elmnt.style.backgroundImage === backgroundImage, elmnt.style.backgroundColor === backgroundColor) {
+// 		elmnt.style.backgroundColor = 'transparent';
+// 		elmnt.style.backgroundImage = 'url(images/like.png)';
+// 	} else {
+// 		elmnt.style.backgroundColor = 'rgb(44, 113, 184)';
+// 		elmnt.style.backgroundImage = 'url(images/likewhite.png)';
+// 	}
+// }
+
+
+function changeLikeOnClick(elmnt, backgroundColor) {
+	if (elmnt.style.backgroundColor === backgroundColor) {
+		elmnt.style.backgroundColor = 'transparent';
+		// elmnt.style.backgroundImage = 'url(images/like.png)';
+	} else {
+		elmnt.style.backgroundColor = 'rgb(44, 113, 184)';
+		// elmnt.style.backgroundImage = 'url(images/likewhite.png)';
+	}
+}
+
+	// var x = document.getElementById('like');
+ //    if (x.style.backgroundColor =='red') {
+ //        // x.style.backgroundImage = 'url(images/likewhite.png)';
+ //        x.style.backgroundColor = 'blue';}
+ //    else {
+ //        // x.style.backgroundImage = 'url(images/like.png)';
+ //        x.style.backgroundColor = 'red';
+ //    }
